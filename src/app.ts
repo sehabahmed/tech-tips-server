129828e90a0879ae635ff0de32657eed52993e33
Sehab Ahmed <sehabahmed50100@gmail.com>
2025-09-12 09:16:20 +0200

    fix: cors origin vercel url added to live the server

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://tech-tips-client.vercel.app", "http://localhost:3000"],
     credentials: true,
   })
 );
